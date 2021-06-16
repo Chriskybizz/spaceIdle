692ccff217490e7f54164c022128bf225f3c820f
Chriskybizz <67864176+Chriskybizz@users.noreply.github.com>
2021-06-16 18:27:16 +0100

    Auto rest
    scale-able auto rest upgrade added.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var number = 0;
 var energy = 175;
+var maxEnergy = 175;
 var ships = 0;
 var clickPower = 1;
 var minePowerCost = Math.floor(25 * Math.pow(2.5, clickPower));
+var restPower = 1;
+var restPowerCost = Math.floor(20 * Math.pow(2.5, restPower));
+var sleepPower = 0;
+var sleepPowerCost = Math.floor(20 * Math.pow(2.5, sleepPower));
 
 
 var numberUp = (val) => {
@@ -36,24 +41,31 @@ var autoShip = () => {
 }
 
 var rest = () => {
-    if(energy < 175)
-    energy += 1;
+    if(energy < maxEnergy)
+    energy += restPower;
     document.getElementById("finish").style.width = energy + "px";
 }
 
+var autoRest = (sleepVal) => {
+    if(energy < maxEnergy){
+    energy += sleepVal;
+    document.getElementById("finish").style.width = energy + "px";
+}
+}
+
 window.setInterval(function(){
-    numberUpAuto(Math.floor(ships)), upgradeUnlock()
+    numberUpAuto(Math.floor(ships)), upgradeUnlock(), autoRest(sleepPower)
  }, 1000
 )
 
 var upgradeUnlock = () => {
-    if (number >= (minePowerCost)) {
+    if (number >= minePowerCost) {
         document.getElementById("upgrade1").style.display = "block";
     }
-    if (number >= 1000) {
+    if (number >= restPowerCost) {
         document.getElementById("upgrade2").style.display = "block";
     }
-    if (number >= 2500) {
+    if (number >= sleepPowerCost) {
         document.getElementById("upgrade3").style.display = "block";
     }
     if (number >= 6000) {
@@ -79,17 +91,39 @@ var upgradeUnlock = () => {
     }
 }
 document.getElementById("minePowerCost").innerHTML = minePowerCost;
+document.getElementById("effectiveRestCost").innerHTML = restPowerCost; 
+document.getElementById("sleepNaniteCost").innerHTML = sleepPowerCost
+
 
 
 var minePower = () => {
+    if(number >= minePowerCost){
     number -= (minePowerCost);
     clickPower += 1;
     document.getElementById("numberValue").innerHTML = number;
     document.getElementById("upgrade1").style.display = "none";
     minePowerCost = Math.floor(25 * Math.pow(2.5, clickPower));
     document.getElementById("minePowerCost").innerHTML = minePowerCost;
+}}
 
+var effectiveRest = () => {
+    if(number >= restPowerCost){
+    number -= restPowerCost;
+    restPower += 1;
+    document.getElementById("numberValue").innerHTML = number;
+    document.getElementById("upgrade2").style.display = "none";
+    restPowerCost = Math.floor(25 * Math.pow(2.5, restPower));
+    document.getElementById("effectiveRestCost").innerHTML = restPowerCost;    
+}}
 
-}
+var sleepNanites = () => {
+    if(number >= sleepPowerCost){
+    number -= sleepPowerCost;
+    sleepPower += 1;
+    document.getElementById("numberValue").innerHTML = number;
+    document.getElementById("upgrade3").style.display = "none";
+    sleepPowerCost = Math.floor(25 * Math.pow(2.5, sleepPower));
+    document.getElementById("sleepNaniteCost").innerHTML = sleepPowerCost ;    
+}}
 
 
